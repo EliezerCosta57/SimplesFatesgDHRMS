b22b2ff59b73f06ea53a411fc7fe770a8e6aaf0c
Eliézer Costa <eliezercosta.ti@gmail.com>
2026-05-14 20:57:45 -0700

    Atividade 3: Implementação de um LoadBalancer para o servidor de calculos

--- web_api/src/main/java/com/fatesg/web_api/apis/ServidorDeCalculoApi.java
+++ web_api/src/main/java/com/fatesg/web_api/apis/ServidorDeCalculoApi.java
@@ -15,18 +15,44 @@ import com.fatesg.web_api.configs.JsonRPCConfig;
 import com.google.gson.Gson;
 
 public class ServidorDeCalculoApi implements ClienteDeCalculoFolhaInterface {
+
     private Socket cliente;
     private PrintWriter saida;
     private BufferedReader entrada;
+
+    // PORTAS DOS SERVIDORES
+    private final int[] portas = { 1198, 1199 };
+
+    // CONTROLE DO LOAD BALANCER
+    private int indiceAtual = 0;
 
     public ServidorDeCalculoApi() {
         super();
     }
 
+    // ESCOLHE A PRÓXIMA PORTA (ROUND ROBIN)
+    private int obterProximaPorta() {
+        int porta = portas[indiceAtual];
+
+        indiceAtual = (indiceAtual + 1) % portas.length;
+
+        System.out.println("Requisição enviada para porta: " + porta);
+
+        return porta;
+    }
+
     private void conectar() throws IOException {
-        this.cliente = new Socket(JsonRPCConfig.JSON_RPC_SERVER_HOST, (int) JsonRPCConfig.JSON_RPC_SERVER_PORT);
+
+        int porta = obterProximaPorta();
+
+        this.cliente = new Socket(
+                JsonRPCConfig.JSON_RPC_SERVER_HOST,
+                porta);
+
         this.saida = new PrintWriter(cliente.getOutputStream(), true);
-        this.entrada = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
+
+        this.entrada = new BufferedReader(
+                new InputStreamReader(cliente.getInputStream()));
     }
 
     private void fecharConexao() throws IOException {
@@ -39,33 +65,42 @@ public class ServidorDeCalculoApi implements ClienteDeCalculoFolhaInterface {
     }
 
     private JsonRpcResponse<Object> enviar(JsonRpcRequest request) throws IOException, Exception {
+
         conectar();
+
         if (!cliente.isConnected())
             throw new IOException("Socket cliente não está conectado!");
 
         String reqJson = new Gson().toJson(request);
+
         this.saida.println(reqJson);
 
         String resJson = "";
         String retorno;
+
         while ((retorno = this.entrada.readLine()) != null) {
             resJson += retorno;
         }
 
         @SuppressWarnings("unchecked")
-        JsonRpcResponse<Object> response = (JsonRpcResponse<Object>) new Gson().fromJson(resJson,
+        JsonRpcResponse<Object> response = (JsonRpcResponse<Object>) new Gson().fromJson(
+                resJson,
                 JsonRpcResponse.class);
 
         fecharConexao();
+
         return response;
     }
 
     @Override
     public FolhaDto calcularFolhaDePagamento(byte mes, short ano) {
         try {
+
             JsonRpcRequest request = new JsonRpcRequest();
+
             request.id = 1;
             request.method = "calcularFolhaDePagamento";
+
             request.params = new String[] {
                     String.valueOf(mes),
                     String.valueOf(ano)
@@ -77,37 +112,52 @@ public class ServidorDeCalculoApi implements ClienteDeCalculoFolhaInterface {
                 throw new Exception(response.error.message);
             }
 
-            if(response.result == null)
+            if (response.result == null)
                 throw new Exception("Resultado nulo");
 
             var parser = new Gson();
+
             String retorno = parser.toJson(response.result);
+
             FolhaDto dto = parser.fromJson(retorno, FolhaDto.class);
+
             return dto;
-            
+
         } catch (IOException e) {
+
             System.err.println("Erro de IOException no calcularFolhaDePagamento: " + e.getMessage());
+
             e.printStackTrace();
+
             return null;
+
         } catch (Exception e) {
+
             System.err.println("Erro no calcularFolhaDePagamento: " + e.getMessage());
+
             e.printStackTrace();
+
             return null;
         }
     }
 
     @Override
     public FolhaDto calcularFolhaDePagamentoDoDepartamento(String arg0, byte arg1, short arg2) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'calcularFolhaDePagamentoDoDepartamento'");
+
+        throw new UnsupportedOperationException(
+                "Unimplemented method 'calcularFolhaDePagamentoDoDepartamento'");
     }
 
     @Override
     public ReciboDto calcularReciboDePagamento(int idFuncionario, byte mes, short ano) {
+
         try {
+
             JsonRpcRequest request = new JsonRpcRequest();
+
             request.id = 2;
             request.method = "calcularReciboDePagamento";
+
             request.params = new String[] {
                     String.valueOf(idFuncionario),
                     String.valueOf(mes),
@@ -125,28 +175,44 @@ public class ServidorDeCalculoApi implements ClienteDeCalculoFolhaInterface {
             }
 
             var parser = new Gson();
+
             String retorno = parser.toJson(response.result);
+
             ReciboDto dto = parser.fromJson(retorno, ReciboDto.class);
+
             return dto;
 
         } catch (IOException e) {
+
             System.err.println("Erro de IOException no calcularReciboDePagamento: " + e.getMessage());
+
             e.printStackTrace();
+
             return null;
+
         } catch (Exception e) {
+
             System.err.println("Erro no calcularReciboDePagamento: " + e.getMessage());
+
             e.printStackTrace();
+
             return null;
         }
     }
 
     @Override
     public double calcularSalarioLiquido(double salarioBruto) {
+
         try {
+
             JsonRpcRequest request = new JsonRpcRequest();
+
             request.id = 2;
             request.method = "calcularSalarioLiquido";
-            request.params = new String[] { String.valueOf(salarioBruto) };
+
+            request.params = new String[] {
+                    String.valueOf(salarioBruto)
+            };
 
             JsonRpcResponse<Object> response = this.enviar(request);
 
@@ -155,16 +221,24 @@ public class ServidorDeCalculoApi implements ClienteDeCalculoFolhaInterface {
             }
 
             Double retorno = (Double) response.result;
+
             return (double) retorno;
+
         } catch (IOException e) {
+
             System.err.println("Erro de IOException no calcularSalarioLiquido: " + e.getMessage());
+
             e.printStackTrace();
+
             return 0.00;
+
         } catch (Exception e) {
+
             System.err.println("Erro no calcularSalarioLiquido: " + e.getMessage());
+
             e.printStackTrace();
+
             return 0.00;
         }
     }
-
 }
